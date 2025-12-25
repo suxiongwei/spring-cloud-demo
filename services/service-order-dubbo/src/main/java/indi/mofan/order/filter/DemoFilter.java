613953229c9e7dd57946e4f96af54550907a9fd5
xiongweisu <xiongweisu@sohu-inc.com>
2025-12-25 20:50:49 +0800

    feat(dubbo): 添加Dubbo过滤器用于记录请求和响应信息
    实现一个Dubbo过滤器，用于拦截请求和响应，记录接口调用信息、参数、执行时间等关键数据

---
+++ services/service-order-dubbo/src/main/java/indi/mofan/order/filter/DemoFilter.java
@@ -0,0 +1,73 @@
+package indi.mofan.product.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Activate(group = {"provider", "consumer"})
+public class DemoFilter implements Filter {
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) {
+        long startTime = System.currentTimeMillis();
+
+        RpcContext rpcContext = RpcContext.getServiceContext();
+        String interfaceName = invoker.getInterface().getName();
+        String methodName = invocation.getMethodName();
+        String remoteAddress = rpcContext.getRemoteHost() + ":" + rpcContext.getRemotePort();
+
+        log.info("=== Dubbo Filter 拦截开始 ===");
+        log.info("接口: {}", interfaceName);
+        log.info("方法: {}", methodName);
+        log.info("远程地址: {}", remoteAddress);
+        log.info("参数类型: {}", Arrays.toString(invocation.getParameterTypes()));
+        log.info("参数值: {}", Arrays.toString(invocation.getArguments()));
+
+        Map<String, Object> requestInfo = new HashMap<>();
+        requestInfo.put("interfaceName", interfaceName);
+        requestInfo.put("methodName", methodName);
+        requestInfo.put("remoteAddress", remoteAddress);
+        requestInfo.put("parameterTypes", Arrays.toString(invocation.getParameterTypes()));
+        requestInfo.put("parameterValues", Arrays.toString(invocation.getArguments()));
+        requestInfo.put("startTime", startTime);
+
+        invocation.getAttachments().put("filter-request-info", requestInfo.toString());
+
+        try {
+            Result result = invoker.invoke(invocation);
+
+            long duration = System.currentTimeMillis() - startTime;
+
+            log.info("=== Dubbo Filter 拦截结束 ===");
+            log.info("执行时间: {}ms", duration);
+            log.info("返回值: {}", result.getValue());
+            log.info("异常: {}", result.getException());
+
+            Map<String, Object> responseInfo = new HashMap<>();
+            responseInfo.put("duration", duration);
+            responseInfo.put("returnValue", result.getValue());
+            responseInfo.put("hasException", result.getException() != null);
+            responseInfo.put("exception", result.getException() != null ? result.getException().getMessage() : null);
+
+            result.getAttachments().put("filter-response-info", responseInfo.toString());
+
+            return result;
+        } catch (Exception e) {
+            long duration = System.currentTimeMillis() - startTime;
+            log.error("Dubbo Filter 拦截异常: {}", e.getMessage(), e);
+
+            Map<String, Object> errorInfo = new HashMap<>();
+            errorInfo.put("duration", duration);
+            errorInfo.put("error", e.getMessage());
+
+            invocation.getAttachments().put("filter-error-info", errorInfo.toString());
+
+            throw e;
+        }
+    }
+}
---
+++ services/service-product-dubbo/src/main/java/indi/mofan/product/filter/DemoFilter.java
@@ -0,0 +1,12 @@
+package filter;
+
+
+/**
+ * @Class: DemoFilter
+ * @Description: // TODO(用一句话描述该文件做什么)
+ * @Author xiongweisu
+ * @Date 2025/12/25 20:17
+ * @Version V1.0
+ */
+public class DemoFilter {
+}
